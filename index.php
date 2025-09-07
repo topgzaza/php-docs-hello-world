de592047ea80961fdcae899731415f5112bd31fe
topgzaza <134916272+topgzaza@users.noreply.github.com>
2025-09-07 18:11:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-$student_id = getenv("STUDENT_ID");
-echo "<h2>Numer indeksu: $student_id</h2>";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP App on Azure</title>
+</head>
+<body>
+  <h1>Hello Azure!</h1>
+  <?php
+    $student_id = getenv("STUDENT_ID");
+    echo "<h2>Numer indeksu: $student_id</h2>";
+  ?>
+</body>
+</html>
